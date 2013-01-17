c8d188318b24c6303a16b545f52a2e0bdfbf3af1
Tom Van Herreweghe <tom@dx-solutions.be>
2013-01-17 21:53:13 +0100

    Updated tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,9 +6,11 @@ ini_set('display_startup_errors', 1);
 
 date_default_timezone_set('Europe/Brussels');
 
-//require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+/*
 require_once realpath(__DIR__ . '/../../autoload.php');
 $classLoader = new SplClassLoader('PHPExif', __DIR__ . '/../lib');
 $classLoader->register();
+ */
 
 define('PHPEXIF_TEST_ROOT', __DIR__);
