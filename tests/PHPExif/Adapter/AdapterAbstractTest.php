2b7fbc6c9521cbe8d1a791a6f63afc4d39b8703f
Tom Van Herreweghe <tom@king-foo.be>
2015-04-01 14:37:42 +0200

    Add extra unit tests for AdapterAbstract
    Signed-off-by: Tom Van Herreweghe <tom@king-foo.be>

--- tests/PHPExif/Adapter/AdapterAbstractTest.php
+++ tests/PHPExif/Adapter/AdapterAbstractTest.php
@@ -82,4 +82,150 @@ class AdapterAbstractTest extends \PHPUnit_Framework_TestCase
             $this->assertEquals($value, $reflProp->getValue($adapter));
         }
     }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::setMapper
+     */
+    public function testSetMapperReturnsCurrentInstance()
+    {
+        $mapper = new \PHPExif\Mapper\Native();
+        $result = $this->adapter->setMapper($mapper);
+        $this->assertSame($this->adapter, $result);
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::setMapper
+     */
+    public function testSetMapperCorrectlySetsInProperty()
+    {
+        $mapper = new \PHPExif\Mapper\Native();
+        $this->adapter->setMapper($mapper);
+
+        $reflProp = new \ReflectionProperty('\\PHPExif\\Adapter\\AdapterAbstract', 'mapper');
+        $reflProp->setAccessible(true);
+        $this->assertSame($mapper, $reflProp->getValue($this->adapter));
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::getMapper
+     */
+    public function testGetMapperCorrectlyReturnsFromProperty()
+    {
+        $mapper = new \PHPExif\Mapper\Native();
+        $reflProp = new \ReflectionProperty('\\PHPExif\\Adapter\\AdapterAbstract', 'mapper');
+        $reflProp->setAccessible(true);
+        $reflProp->setValue($this->adapter, $mapper);
+        $this->assertSame($mapper, $this->adapter->getMapper());
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::getMapper
+     */
+    public function testGetMapperLazyLoadsMapperWhenNotPresent()
+    {
+        $reflProp = new \ReflectionProperty(
+            get_class($this->adapter),
+            'mapperClass'
+        );
+
+        $mapperClass = '\\PHPExif\\Mapper\\Native';
+        $reflProp->setAccessible(true);
+        $reflProp->setValue($this->adapter, $mapperClass);
+
+        $this->assertInstanceOf($mapperClass, $this->adapter->getMapper());
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::getMapper
+     */
+    public function testGetMapperLazyLoadingSetsInProperty()
+    {
+        $reflProp = new \ReflectionProperty(
+            get_class($this->adapter),
+            'mapperClass'
+        );
+
+        $mapperClass = '\\PHPExif\\Mapper\\Native';
+        $reflProp->setAccessible(true);
+        $reflProp->setValue($this->adapter, $mapperClass);
+
+        $reflProp2 = new \ReflectionProperty(
+            get_class($this->adapter),
+            'mapper'
+        );
+        $reflProp2->setAccessible(true);
+        $this->adapter->getMapper();
+        $this->assertInstanceOf($mapperClass, $reflProp2->getValue($this->adapter));
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::setHydrator
+     */
+    public function testSetHydratorReturnsCurrentInstance()
+    {
+        $hydrator = new \PHPExif\Hydrator\Mutator();
+        $result = $this->adapter->setHydrator($hydrator);
+        $this->assertSame($this->adapter, $result);
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::setHydrator
+     */
+    public function testSetHydratorCorrectlySetsInProperty()
+    {
+        $hydrator = new \PHPExif\Hydrator\Mutator();
+        $this->adapter->setHydrator($hydrator);
+
+        $reflProp = new \ReflectionProperty('\\PHPExif\\Adapter\\AdapterAbstract', 'hydrator');
+        $reflProp->setAccessible(true);
+        $this->assertSame($hydrator, $reflProp->getValue($this->adapter));
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::getHydrator
+     */
+    public function testGetHydratorCorrectlyReturnsFromProperty()
+    {
+        $hydrator = new \PHPExif\Hydrator\Mutator();
+        $reflProp = new \ReflectionProperty('\\PHPExif\\Adapter\\AdapterAbstract', 'hydrator');
+        $reflProp->setAccessible(true);
+        $reflProp->setValue($this->adapter, $hydrator);
+        $this->assertSame($hydrator, $this->adapter->getHydrator());
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::getHydrator
+     */
+    public function testGetHydratorLazyLoadsHydratorWhenNotPresent()
+    {
+        $hydratorClass = '\\PHPExif\\Hydrator\\Mutator';
+        $this->assertInstanceOf($hydratorClass, $this->adapter->getHydrator());
+    }
+
+    /**
+     * @group adapter
+     * @covers \PHPExif\Adapter\AdapterAbstract::getHydrator
+     */
+    public function testGetHydratorLazyLoadingSetsInProperty()
+    {
+        $hydratorClass = '\\PHPExif\\Hydrator\\Mutator';
+
+        $reflProp = new \ReflectionProperty(
+            get_class($this->adapter),
+            'hydrator'
+        );
+        $reflProp->setAccessible(true);
+        $this->adapter->getHydrator();
+        $this->assertInstanceOf($hydratorClass, $reflProp->getValue($this->adapter));
+    }
 }
+
