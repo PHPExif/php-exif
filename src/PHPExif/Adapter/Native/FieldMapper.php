2cb901288eb01e6abd3d1304f45a21398b991fbf
Tom Van Herreweghe <tom@theanalogguy.be>
2016-02-29 22:27:00 +0100

    ArrayMapper & FieldMapper interfaces
    Signed-off-by: Tom Van Herreweghe <tom@theanalogguy.be>

---
+++ src/PHPExif/Adapter/Native/FieldMapper.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * PHP Exif FieldMapper Interface: Defines the interface for field data mappers
+ *
+ * @link        http://github.com/miljar/PHPExif for the canonical source repository
+ * @copyright   Copyright (c) 2015 Tom Van Herreweghe <tom@theanalogguy.be>
+ * @license     http://github.com/miljar/PHPExif/blob/master/LICENSE MIT License
+ * @category    PHPExif
+ * @package     Mapper
+ * @codeCoverageIgnore
+ */
+
+namespace PHPExif\Adapter\Native;
+
+use PHPExif\Data\MetadataInterface;
+
+/**
+ * FieldMapper
+ *
+ * Public API for mapping fields of EXIF data
+ * to and from Exif
+ *
+ * @category    PHPExif
+ * @package     Mapper
+ */
+interface FieldMapper
+{
+    /**
+     * Returns a list of fields this instance knows how to map
+     *
+     * @return array
+     */
+    public function getSupportedFields();
+
+    /**
+     * Map given $field from given input to given output
+     *
+     * @param string $field
+     * @param aray $input
+     * @param object $output
+     *
+     * @return void
+     */
+    public function mapField($field, array $input, &$output);
+}
