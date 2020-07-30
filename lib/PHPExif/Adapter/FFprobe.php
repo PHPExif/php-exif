3893a3030ec665e8433f67e2ec9d28836d894bdb
tmp-hallenser <1869257+tmp-hallenser@users.noreply.github.com>
2020-07-30 15:16:10 +0200

    Remove mimetype fix from code coverage calculation

--- lib/PHPExif/Adapter/FFprobe.php
+++ lib/PHPExif/Adapter/FFprobe.php
@@ -95,7 +95,9 @@ class FFprobe extends AdapterAbstract
         $mimeType = mime_content_type($file);
 
         if ($mimeType === 'image/x-tga') {
+            // @codeCoverageIgnoreStart
             $mimeType = 'video/mpeg';
+            // @codeCoverageIgnoreEnd
         }
 
         // file is not a video -> wrong adapter
